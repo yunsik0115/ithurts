3f6897ca35b7c205b744b531f59de2f00571bec4
Yunsik Kim <yunsik0115@uos.ac.kr>
2023-11-02 14:21:47 +0900

    Initial Commit For Side Project " it hurts "

---
+++ src/main/java/com/sidepj/ithurts/IthurtsApplication.java
@@ -0,0 +1,13 @@
+package com.sidepj.ithurts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IthurtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IthurtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidepj/ithurts/IthurtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidepj.ithurts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IthurtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
