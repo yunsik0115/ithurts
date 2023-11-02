c883e4c51546aba43aa1cd73f1a1b1ea9b3ee02e
Yunsik Kim <yunsik0115@uos.ac.kr>
2023-11-02 15:59:07 +0900

    Pharmacy Entity Creation & Hibernate Spatial Settings for coordinates

---
+++ src/main/java/com/sidepj/ithurts/domain/Pharmacy.java
@@ -0,0 +1,34 @@
+package com.sidepj.ithurts.domain;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Point;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Pharmacy {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private String contact;
+
+    private String Address; //Embedded Class Address will be made - TO DO -
+
+    private String officeDay;
+
+    private String officeTime;
+
+    private Point coordinates;
+
+    private LocalDateTime updatedOn;
+    private LocalDateTime createdOn;
+
+    private boolean is_available;
+}
