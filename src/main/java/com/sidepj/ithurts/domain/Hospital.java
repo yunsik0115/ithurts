6de05239edae3ca74d2b680cda60ee7cb4f35057
Yunsik Kim <yunsik0115@uos.ac.kr>
2023-11-03 12:08:26 +0900

    Hospital Entity Column Naming

--- src/main/java/com/sidepj/ithurts/domain/Hospital.java
+++ src/main/java/com/sidepj/ithurts/domain/Hospital.java
@@ -18,22 +18,32 @@ public class Hospital {
     @Column(name="hospital_id")
     private int id;
 
+    @Column(name="hospital_name")
     private String name;
 
+    @Column(name="hospital_contact")
     private String contact;
 
+    @Column(name="hospital_address")
     private String Address; //Embedded Class Address will be made - TO DO -
 
+    @Column(name="hospital_type")
     private String hospitalType;
 
+    @Column(name="hospital_officeday")
     private String officeDay;
 
+    @Column(name = "hospital_officetime")
     private String officeTime;
 
+    @Column(name = "hospital_coordinates")
     private Point coordinates;
 
+    @Column(name = "hospital_updated_date")
     private LocalDateTime updatedOn;
+    @Column(name = "hospital_created_date")
     private LocalDateTime createdOn;
+
 
     private boolean is_available;
 
