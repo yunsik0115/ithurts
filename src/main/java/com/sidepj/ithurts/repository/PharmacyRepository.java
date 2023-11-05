fb3f522f33532eeadff9765de2aff9cd9922fa6a
Yunsik Kim <yunsik0115@uos.ac.kr>
2023-11-05 18:54:30 +0900

    PharmacyRepository Creation

---
+++ src/main/java/com/sidepj/ithurts/repository/PharmacyRepository.java
@@ -0,0 +1,8 @@
+package com.sidepj.ithurts.repository;
+
+import com.sidepj.ithurts.domain.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+}
