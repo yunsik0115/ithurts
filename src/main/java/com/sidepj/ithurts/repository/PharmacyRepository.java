be001fa6ce1ddb37c08931232226f7ca91d74b80
Yunsik Kim <yunsik0115@uos.ac.kr>
2023-12-03 16:50:12 +0900

    Pharmacy & Hospital Find By Radius + Dialect Change which Supports Spatial Function

--- src/main/java/com/sidepj/ithurts/repository/PharmacyRepository.java
+++ src/main/java/com/sidepj/ithurts/repository/PharmacyRepository.java
@@ -1,13 +1,20 @@
 package com.sidepj.ithurts.repository;
 
+import com.sidepj.ithurts.domain.Hospital;
 import com.sidepj.ithurts.domain.Pharmacy;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+    @Query("SELECT p FROM Pharmacy p WHERE FUNCTION('ST_DISTANCE_SPHERE', p.coordinates, :point) < :distance ")
+    public List<Pharmacy> findByRadius(@Param("point") Point point , @Param("distance") double distance);
     public List<Pharmacy> findByName(String name);
 
     public List<Pharmacy> findByAddressContains(String address);
